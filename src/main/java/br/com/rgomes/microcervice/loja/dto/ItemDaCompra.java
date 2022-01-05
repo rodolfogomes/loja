72d0b9b47aa9981c4be091bd95d0e28df8c3ba1a
Rodolfo Gomes <rodolfoaguiargomes@gmail.com>
2022-01-05 09:54:45 -0300

    first commit

---
+++ src/main/java/br/com/rgomes/microcervice/loja/dto/ItemDaCompra.java
@@ -0,0 +1,24 @@
+package br.com.rgomes.microcervice.loja.dto;
+
+public class ItemDaCompra {
+
+    private Long id;
+
+    private int quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
